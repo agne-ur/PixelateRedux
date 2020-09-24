3fdc851e26bf782dac7ad9687745d405317a2840
Danny Berger <danberadi@gmail.com>
2020-09-24 18:18:20 -0400

    Added more features: clear, reset, add column, fill, and fill all grey. Added more color options.

--- script.js
+++ script.js
@@ -1,63 +1,121 @@
 // Your code here
 const addRow = document.getElementById("add-row");
+const addColumn = document.getElementById("add-column");
 const table = document.getElementsByTagName("table")[0];
 const select = document.getElementsByTagName("select")[0];
 const clear = document.getElementById("clear");
-let color = 'red';
+const fillAll = document.getElementById("fill-all");
+const fillGrey = document.getElementById("fill-grey");
+const columnSelecter = document.getElementsByTagName("select")[1];
+const reset = document.getElementById('reset');
 
+let color = 'red';
+let columns = 7;
+
+// Pass the parent node in as an argument and append a new cell right onto it
+function makeCell(parent) {
+  const cell = document.createElement('td');
+    cell.setAttribute('class', 'default')
+    parent.appendChild(cell);
+}
+
+// Invoke makeCell inside of for loop, and then appends the new row onto table
 function makeRow() {
   const row = document.createElement('tr')
-
-  for (let i = 0; i < 15; i++) {
-    const cell = document.createElement('td');
-    cell.setAttribute('class', 'default')
-    row.appendChild(cell);
+  for (let i = 0; i < columns; i++) {
+    makeCell(row);
   }
-
   table.appendChild(row);
 }
 
+function makeColumn() {
+  const rows = Array.from(table.childNodes);
+
+  rows.forEach(row => {
+    if (row.nodeName === "TR") {
+      makeCell(row)
+    }
+  })
+
+  // for (let i = 1; i < rows.length; i++) {
+  //   makeCell(rows[i])
+  // }
+}
+
 function colorize (event) {
-  if (event.target.tagName === 'TD') {
-    const tdCell = event.target;
-    if (tdCell.className !== 'default') {
+  const tdCell = event.target;
+  if (tdCell.tagName === 'TD') {
+    if (tdCell.className !== 'default' && tdCell.className === color) {
         tdCell.className = 'default'
     } else {
         tdCell.className = color;
-        console.dir(tdCell)
     }
   }
 }
 
-function clearPalette() {
-  const rows = Array.from(table.childNodes)
+function getCells() {
+  let cellsArr = [];
+  const rows = Array.from(table.childNodes);
   rows.forEach(row => {
     const cells = Array.from(row.childNodes)
-    cells.forEach(cell => {
-      cell.className = 'default';
-    })
+    cellsArr = cellsArr.concat(cells)
   })
+  return cellsArr;
+}
+
+function clearPalette() {
+  const cells = getCells()
+  cells.forEach(cell => {
+    cell.className = 'default';
+  })
+}
+
+function fillAllCells() {
+  const cells = getCells()
+  cells.forEach(cell => {
+    cell.className = color;
+  })
+}
+
+function fillGreyCells() {
+  const cells = getCells()
+  cells.forEach(cell => {
+    if (cell.className === 'default') {
+      cell.className = color;
+    }
+  })
+}
+
+function resetBoard() {
+  table.innerHTML = '';
 }
 
 select.addEventListener("change", function(event) {
   color = event.target.value;
 })
 
-
-addRow.addEventListener("click", function() {
-  makeRow();
+columnSelecter.addEventListener("change", function(event) {
+  columns = Number(event.target.value);
 })
 
+addRow.addEventListener("click", makeRow)
+
+addColumn.addEventListener("click", makeColumn)
+
 table.addEventListener('mouseup', function() {
-    table.removeEventListener('mouseover', colorize )
+  table.removeEventListener('mouseover', colorize )
 })
 
 table.addEventListener('mousedown', function() {
-    table.addEventListener('mouseover', colorize )
+  table.addEventListener('mouseover', colorize )
 })
-
-//table.addEventListener('mousemove', colorize )
 
 table.addEventListener('click', colorize )
 
 clear.addEventListener('click', clearPalette)
+
+fillAll.addEventListener('click', fillAllCells)
+
+fillGrey.addEventListener('click', fillGreyCells)
+
+reset.addEventListener('click', resetBoard)
