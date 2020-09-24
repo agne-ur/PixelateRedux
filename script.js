069459e09a3aac54751a68a996e8ed64435a3d2e
Danny Berger <danberadi@gmail.com>
2020-09-24 12:04:23 -0400

    add row feature is up and working

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Your code here
+const addRow = document.getElementById("add-row");
+const table = document.getElementsByTagName("table")[0];
+
+function makeRow() {
+  const row = document.createElement('tr')
+
+  for (let i = 0; i < 15; i++) {
+    const cell = document.createElement('td');
+    row.appendChild(cell);
+  }
+
+  table.appendChild(row);
+}
+
+addRow.addEventListener("click", function() {
+  makeRow();
+})
+
