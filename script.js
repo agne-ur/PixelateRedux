02ffd87898dbaad98ebf597a88def0e471f3e3ad
Danny Berger <danberadi@gmail.com>
2020-09-24 13:29:28 -0400

    Added change color feature from drop down selector

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Your code here
 const addRow = document.getElementById("add-row");
 const table = document.getElementsByTagName("table")[0];
+const select = document.getElementsByTagName("select")[0];
+let color = 'red';
 
 function makeRow() {
   const row = document.createElement('tr')
@@ -13,17 +15,21 @@ function makeRow() {
   table.appendChild(row);
 }
 
+function colorize (event) {
+  const tdCell = event.target;
+  if (tdCell.className.length) {
+      tdCell.className = ''
+  } else {
+      tdCell.className = color;
+  }
+}
+
+select.addEventListener("change", function(event) {
+  color = event.target.value;
+})
+
 addRow.addEventListener("click", function() {
   makeRow();
 })
-
-function colorize (event) {
-    const tdCell = event.target;
-    if (tdCell.className.length) {
-        tdCell.className = ''
-    } else {
-        tdCell.className = 'red'
-    }
-}
 
 table.addEventListener('click', colorize )
